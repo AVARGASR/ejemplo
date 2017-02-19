838fc063530df3a94b2995a846aa10dbfab0aed9
avargasr <avargasutem@gmail.com>
2017-02-19 18:59:43 -0300

    Subiendo cambios

--- app/models/ayudante.rb
+++ app/models/ayudante.rb
@@ -2,10 +2,14 @@ class Ayudante < ActiveRecord::Base
 
 	validates_format_of :nombre, :with => /\A[a-zA-Z\s]+\z/ , allow_blank: true
 	validates :nombre , :presence => {:message => "Campo obligatorio"}
+	validates_format_of :apellido, :with => /\A[a-zA-Z\s]+\z/ , allow_blank: true
+	validates :apellido , :presence => {:message => "Campo obligatorio"}
 	validates :rut, :uniqueness => {:message => "ya ingresado anteriormente"} , allow_blank: true
-	validates :rut, :presence => {:message => "Campo vacio. Usted debe ingresar un rut"}
+	validates :rut, :presence => {:message => "Campo obligatorio"}
 	validates :rut, :rut => {:message => "no es valido"} , allow_blank: true
 	validates :email , :presence => {:message => "Campo obligatorio"}
 	validates :email, email_format: { message: "no es correcto" } , allow_blank: true
+	validates :asignatura, :presence => {:message => "tiene campo vacio"}
+	validates :carrera, :presence => {:message => "tiene campo vacio"}
 	
 end
